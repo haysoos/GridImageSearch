83f1d177f10203d9ee68e8f1961d601098120b6b
Jesus Medrano <jesusimedrano@yahoo.com>
2013-06-16 15:01:13 -0700

    Meets minimum requirements for homework #2

--- GridImageSearch/src/com/codepath/gridimagesearch/SearchActivity.java
+++ GridImageSearch/src/com/codepath/gridimagesearch/SearchActivity.java
@@ -11,14 +11,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -26,9 +31,16 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class SearchActivity extends Activity {
 
+	private static final int NUMBER_OF_IMAGES_PER_REQUEST = 8;
 	Button btnSearch;
 	EditText etQuery;
 	GridView gvResults;
+	EditText etSiteFilter;
+	Spinner spnImageSize;
+	Spinner spnColorFilter;
+	Spinner spnImageType;
+	Button btnMore;
+	private int startIndex = 0;
 	private List<ImageResult> imageResults = new ArrayList<ImageResult>();
 	private ImageResultArrayAdapter imageAdapter;
 	
@@ -43,6 +55,13 @@ public class SearchActivity extends Activity {
 		btnSearch = (Button) this.findViewById(R.id.btnSearch);
 		etQuery = (EditText) this.findViewById(R.id.etQuery);
 		gvResults = (GridView) this.findViewById(R.id.gvResults);
+		etSiteFilter = (EditText) findViewById(R.id.etSiteFilter);
+		spnImageSize = (Spinner) findViewById(R.id.spnImageSize);
+		spnColorFilter = (Spinner) findViewById(R.id.spnColorFilter);
+		spnImageType = (Spinner) findViewById(R.id.spnImageType);
+		btnMore = (Button) findViewById(R.id.btMore);
+		btnMore.setVisibility(View.INVISIBLE);
+		
 		imageAdapter = new ImageResultArrayAdapter(this, imageResults);
 		gvResults.setAdapter(imageAdapter);
 		gvResults.setOnItemClickListener(new OnItemClickListener(){
@@ -57,6 +76,56 @@ public class SearchActivity extends Activity {
 			}
 			
 		});
+		
+		spnImageSize.setOnItemSelectedListener(new SpinnerOnItemClickListener());
+		spnImageType.setOnItemSelectedListener(new SpinnerOnItemClickListener());
+		spnColorFilter.setOnItemSelectedListener(new SpinnerOnItemClickListener());
+		etQuery.addTextChangedListener(new EditTextChangedListener());
+		etSiteFilter.addTextChangedListener(new EditTextChangedListener());
+		
+	}
+	
+	/**
+	 * Needed to reset the start index if a user changes a setting after
+	 * a query had already been requested.
+	 */
+	class SpinnerOnItemClickListener implements OnItemSelectedListener {
+
+		@Override
+		public void onItemSelected(AdapterView<?> adapter, View parent, int position,
+				long rowId) {
+			startIndex = 0;
+			btnMore.setVisibility(View.INVISIBLE);
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+
+		}
+		
+	}
+	
+	class EditTextChangedListener implements TextWatcher {
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			startIndex = 0;
+			btnMore.setVisibility(View.INVISIBLE);			
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+			
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			
+		}
+
+		
 	}
 
 	@Override
@@ -66,12 +135,30 @@ public class SearchActivity extends Activity {
 		return true;
 	}
 
+	public void moreButtonClicked(View v){
+		queryThumbnailImages();
+	}
+	
 	public void onImageSearch(View view){
+		btnMore.setVisibility(View.VISIBLE);
+		startIndex = 0;
+		queryThumbnailImages();
+		
+	}
+
+	private void queryThumbnailImages() {
 		String query = etQuery.getText().toString();
 		//String queryRequest = "http://www.flickr.com/services/rest/?method=flickr.test.echo&format=json&api_key=f3554c0bd142560b90bb9ca268650738";
 		Toast.makeText(this, "Searching for " + query, Toast.LENGTH_SHORT).show();
 		
-		String queryRequest = "https://ajax.googleapis.com/ajax/services/search/images?q=" + Uri.encode(query) + "&rsz=8&v=1.0&start=0";
+		String siteFilter = etSiteFilter.getText().toString();
+		String imageColor = spnColorFilter.getSelectedItem().toString();
+		String imageType = spnImageType.getSelectedItem().toString();
+		String imageSize = spnImageSize.getSelectedItem().toString();
+		
+		String queryRequest = "https://ajax.googleapis.com/ajax/services/search/images?q=" + Uri.encode(query) + 
+				"&rsz=" + NUMBER_OF_IMAGES_PER_REQUEST +"&v=1.0&start=" + startIndex + "&as_sitesearch=" + Uri.encode(siteFilter) + "&imgcolor=" + Uri.encode(imageColor) +
+				"&imgsz=" + Uri.encode(imageSize) + "&imgtype=" + Uri.encode(imageType);
 		AsyncHttpClient client = new AsyncHttpClient();
 		client.get(queryRequest, new JsonHttpResponseHandler(){
 			
@@ -89,6 +176,7 @@ public class SearchActivity extends Activity {
 			}
 		});
 		
+		startIndex += NUMBER_OF_IMAGES_PER_REQUEST;
 	}
 	
 	private void loadFlickrResults(String query){
@@ -104,7 +192,7 @@ public class SearchActivity extends Activity {
 			n	small, 320 on longest side
 			-	medium, 500 on longest side
 			z	medium 640, 640 on longest side
-			c	medium 800, 800 on longest side 
+			c	medium 800, 800 on longest sideï¿½
 			b	large, 1024 on longest side*
 			o	original image, either a jpg, gif or png, depending on source format
 		 */
